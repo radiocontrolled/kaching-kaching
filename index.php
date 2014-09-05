db17098f38e9248bdb7b90565a16d8530930287b
radiocontrolled <radiocontrolled@gmail.com>
2014-09-05 15:22:18 +0100

    load scripts in the footer

--- index.php
+++ index.php
@@ -8,33 +8,30 @@
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-	<?php include 'request.php'; ?>
-	
-	<section>
-		<main>
-		</main>
-	
-		<form id="currencyInputForm">
-	
-			<fieldset class="select-styling">
-				
-				<div class="panel" id="display"></div>
-				
-				<input type="number" id="amountInput" name="name" placeholder="Amount">
-			
-				<label for="first-currency">From</label>
-		  		<select name="first-currency" id="first-currency"></select>
-		  	
-				<label for="second-currency">To</label>
-				<select name="second-currency" id="second-currency"></select>	
-				
-				<input type="submit" value="Convert" id="searchButton">	
-				
-			</fieldset>
-			
-		</form>
+		<section>
+			<main>
+			</main>
+			<form id="currencyInputForm">
 		
-	</section>
-
-<script src="converter.js"></script>
+				<fieldset class="select-styling">
+					
+					<div class="panel" id="display"></div>
+					
+					<input type="number" id="amountInput" name="name" placeholder="Amount">
+				
+					<label for="first-currency">From</label>
+			  		<select name="first-currency" id="first-currency"></select>
+			  	
+					<label for="second-currency">To</label>
+					<select name="second-currency" id="second-currency"></select>	
+					
+					<input type="submit" value="Convert" id="searchButton">	
+					
+				</fieldset>
+			</form>
+		</section>
+	<footer>
+		<?php include 'request.php'; ?>
+		<script src="converter.js"></script>	
+	</footer>
 </body>
